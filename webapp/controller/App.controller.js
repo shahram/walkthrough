88fd5e65948ed8fddf578a18a1337afe36d14fab
shahram <shahram.shahdadi@gmail.com>
2022-01-01 19:22:56 +0100

    Added MessageToast and removed JavaScript alert.

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,14 +1,17 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
 ], function (
-    Controller
+    Controller,
+    MessageToast
 ) {
     "use strict";
 
     return Controller.extend("de.jlabs.ui5.controller.App", {
         onShowHello: function () {
             // Show a native JavaScript alert
-            alert("Hello Jupiter")
+            // alert("Hello Jupiter")
+            MessageToast.show("Hello Jupiter or Mars!");
         }
     });
 });
