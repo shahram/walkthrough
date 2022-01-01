20ee05e9de3b873bd7517ab04d5c7c491a6566fb
shahram <shahram.shahdadi@gmail.com>
2022-01-01 20:16:14 +0100

    Added Input control and data binding

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,13 +1,28 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
 ], function (
     Controller,
-    MessageToast
+    MessageToast,
+    JSONModel,
 ) {
     "use strict";
 
     return Controller.extend("de.jlabs.ui5.controller.App", {
+        /**
+         * @override
+         */
+        onInit: function () {
+            // Controller.prototype.onInit.apply(this, arguments);
+            var oData = {
+                recipient: {
+                    name: "World"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+        },
         onShowHello: function () {
             // Show a native JavaScript alert
             // alert("Hello Jupiter")
