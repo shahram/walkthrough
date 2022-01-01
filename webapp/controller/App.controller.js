d38b0a4c8159b6e29969a7ec0ea8811364de5d05
shahram <shahram.shahdadi@gmail.com>
2022-01-01 20:55:52 +0100

    Added translation resources

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,13 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
 ], function (
     Controller,
     MessageToast,
     JSONModel,
+    ResourceModel,
 ) {
     "use strict";
 
@@ -15,6 +17,7 @@ sap.ui.define([
          */
         onInit: function () {
             // Controller.prototype.onInit.apply(this, arguments);
+            // set data model on view
             var oData = {
                 recipient: {
                     name: "World"
@@ -22,11 +25,22 @@ sap.ui.define([
             };
             var oModel = new JSONModel(oData);
             this.getView().setModel(oModel);
+            // set i18n model on view
+            var i18nModel = new ResourceModel({
+                bundleName: "de.jlabs.ui5.i18n.i18n"
+            });
+            this.getView().setModel(i18nModel, "i18n");
         },
         onShowHello: function () {
+
             // Show a native JavaScript alert
             // alert("Hello Jupiter")
-            MessageToast.show("Hello Jupiter or Mars!");
+            // read msg from i18n model
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+            // show message
+            MessageToast.show(sMsg);
         }
     });
 });
