6fe6d622aff86c4f9dcbda6333e8b1d0aac4e6b5
shahram <shahram.shahdadi@gmail.com>
2022-01-04 22:16:39 +0100

    Introduces manifest file and refactored.

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,24 +1,25 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/model/json/JSONModel",
-    "sap/base/i18n/ResourceBundle",
-    "sap/ui/model/resource/ResourceModel"
+    // "sap/base/i18n/ResourceBundle"
+    // "sap/ui/model/resource/ResourceModel"
 ], function (
     UIComponent,
     JSONModel,
-    ResourceBundle,
-    ResourceModel
+    // ResourceBundle,
+    // ResourceModel
 ) {
     "use strict";
     return UIComponent.extend("de.jlabs.ui5.Component", {
 
         metadata: {
             "interfaces": ["IAsyncContentCreation"],
-            "rootView": {
-                "viewName": "de.jlabs.ui5.view.App",
-                "type": "XML",
-                "id": "app"
-            }
+            // "rootView": {
+            //     "viewName": "de.jlabs.ui5.view.App",
+            //     "type": "XML",
+            //     "id": "app"
+            // }
+            manifest: "json"
         },
 
         /**
@@ -35,11 +36,11 @@ sap.ui.define([
             var oModel = new JSONModel(oData);
             this.setModel(oModel);
 
-            // set i18 Model
-            var i18Model = new ResourceModel({
-                bundleName: "de.jlabs.ui5.i18n.i18n"
-            });
-            this.setModel(i18Model, "i18n");
+            // // set i18 Model
+            // var i18Model = new ResourceModel({
+            //     bundleName: "de.jlabs.ui5.i18n.i18n"
+            // });
+            // this.setModel(i18Model, "i18n");
         }
     });
 });
