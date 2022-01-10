407a720e02ad819418c30949c1e51216e9d92ee1
shahram <shahram.shahdadi@gmail.com>
2022-01-10 15:20:59 +0100

    ST24 added filtering (Filtering)

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,12 +1,15 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "../model/formatter"
-
+    "../model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ], function (
     Controller,
     JSONModel,
-    formatter
+    formatter,
+    Filter,
+    FilterOperator
 ) {
     "use strict";
 
@@ -17,6 +20,18 @@ sap.ui.define([
                 currency: "EUR"
             });
             this.getView().setModel(oViewModel, "view");
+        },
+        onFilterInvoices: function (oEvent) {
+            // build filter array
+            var aFilter = [];
+            var sQuery = oEvent.getParameter("query");
+            if (sQuery) {
+                aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+            }
+            // filter binding
+            var oList = this.byId("invoiceList");
+            var oBinding = oList.getBinding("items");
+            oBinding.filter(aFilter);
         }
     });
 });
